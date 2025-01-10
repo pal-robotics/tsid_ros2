948d9d7eed7a267f9f14b684f7a75756bab1ac2f
ileniaperrella <ilenia.perrella@pal-robotics.com>
2025-01-10 12:36:17 +0100

    joint space controller

---
+++ tsid_controllers/include/tsid_controllers/joint_space_controller.hpp
@@ -0,0 +1,109 @@
+// Copyright (c) 2024 PAL Robotics S.L. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef JOINT_SPACE_TISD_CONTROLLER_HPP_
+#define JOINT_SPACE_TISD_CONTROLLER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+#include <controller_interface/controller_interface.hpp>
+#include <hardware_interface/actuator_interface.hpp>
+#include "hardware_interface/component_parser.hpp"
+#include "pal_utils/better_enums.hpp"
+#include <tsid_controllers_params.hpp>
+#include <pinocchio/fwd.hpp>
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/multibody/data.hpp>
+#include <tsid/robots/robot-wrapper.hpp>
+#include <tsid/formulations/inverse-dynamics-formulation-acc-force.hpp>
+#include <tsid/solvers/solver-HQP-eiquadprog.hpp>
+#include <tsid/solvers/solver-HQP-eiquadprog-rt.hpp>
+#include <tsid/solvers/solver-HQP-eiquadprog-fast.hpp>
+#include <tsid/tasks/task-joint-posture.hpp>
+#include <tsid/tasks/task-joint-posVelAcc-bounds.hpp>
+#include <tsid/tasks/task-joint-bounds.hpp>
+#include <tsid/trajectories/trajectory-euclidian.hpp>
+#include "std_msgs/msg/float64_multi_array.hpp"
+
+
+namespace tsid_controllers
+{
+BETTER_ENUM(
+  Interfaces, int, position = 0, velocity = 1, effort = 2);
+
+
+class JointSpaceTsidController
+  : public controller_interface::ControllerInterface
+{
+public:
+  JointSpaceTsidController();
+
+
+  controller_interface::CallbackReturn on_init() override;
+  controller_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
+
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+  controller_interface::return_type update(
+    const rclcpp::Time & time,
+    const rclcpp::Duration & period) override;
+
+  controller_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state)
+  override;
+
+  controller_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state)
+  override;
+
+  void setPositionCb(
+    std_msgs::msg::Float64MultiArray::ConstSharedPtr msg);
+
+protected:
+  tsid_controllers::Params params_;
+  std::shared_ptr<tsid_controllers::ParamListener> param_listener_;
+  std::vector<std::vector<std::string>> state_interface_names_;
+
+  template<typename T>
+  using InterfaceReferences = std::vector<std::vector<std::reference_wrapper<T>>>;
+  InterfaceReferences<hardware_interface::LoanedStateInterface> joint_state_interfaces_;
+
+private:
+  std::map<std::string, int> jnt_id_;
+  bool first_update_ = true;
+  bool tuning_mode_ = false;
+  pinocchio::Model model_;
+  pinocchio::Data data_;
+  tsid::robots::RobotWrapper * robot_wrapper_;
+  tsid::InverseDynamicsFormulationAccForce * formulation_;
+  tsid::solvers::SolverHQuadProgFast * solver_;
+  tsid::tasks::TaskJointPosture * task_joint_posture_;
+  tsid::tasks::TaskJointBounds * task_joint_bounds_;
+  tsid::trajectories::TrajectoryEuclidianConstant * traj_joint_posture_;
+  rclcpp::Duration dt_;
+  rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr joint_cmd_sub_;
+  std::vector<double> desired_pose_;
+  double v_scaling_;
+
+
+  void updateParams();
+
+
+};
+}
+
+#endif  // JOINT_SPACE_TISD_CONTROLLER_HPP_
---
+++ tsid_controllers/src/joint_space_controller.cpp
@@ -0,0 +1,357 @@
+// Copyright (c) 2024 PAL Robotics S.L. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tsid_controllers/joint_space_controller.hpp"
+#include <pluginlib/class_list_macros.hpp>
+#include "controller_interface/helpers.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+#include <pinocchio/parsers/urdf.hpp>
+#include <pinocchio/algorithm/compute-all-terms.hpp>
+#include <pinocchio/algorithm/model.hpp>
+#include <pinocchio/algorithm/joint-configuration.hpp>
+
+using namespace controller_interface;
+
+namespace tsid_controllers
+{
+using std::placeholders::_1;
+
+JointSpaceTsidController::JointSpaceTsidController()
+: controller_interface::ControllerInterface(),
+  dt_(0, 0)
+{
+}
+
+controller_interface::CallbackReturn JointSpaceTsidController::on_init()
+{
+  try {
+    param_listener_ = std::make_shared<tsid_controllers::ParamListener>(get_node());
+
+    if (!param_listener_) {
+      RCLCPP_ERROR(get_node()->get_logger(), "Failed to initialize ParamListener.");
+      return controller_interface::CallbackReturn::ERROR;
+    }
+    params_ = param_listener_->get_params();
+  } catch (const std::exception & e) {
+    RCLCPP_ERROR(
+      get_node()->get_logger(), "Exception thrown during controller's init: %s",
+      e.what());
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn JointSpaceTsidController::on_configure(
+  const rclcpp_lifecycle::State & /*prev_state*/)
+{
+  // Check if parameters were taken correctly
+  if (!param_listener_) {
+    RCLCPP_ERROR(get_node()->get_logger(), "Error encountered during init");
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  param_listener_->refresh_dynamic_parameters();
+  params_ = param_listener_->get_params();
+
+  // Check if the actuator names are not empty
+  if (params_.joint_names.empty()) {
+    RCLCPP_ERROR(get_node()->get_logger(), "The joints name cannot be empty");
+    return controller_interface::CallbackReturn::ERROR;
+  }
+
+  // Create the state and command interfaces
+  state_interfaces_.reserve(3 * params_.joint_names.size());
+  command_interfaces_.reserve(params_.joint_names.size());
+  joint_state_interfaces_.resize(params_.joint_names.size());
+  state_interface_names_.resize(params_.joint_names.size());
+
+  int idx = 0;
+
+  Interfaces pos_iface = Interfaces::position;
+  Interfaces vel_iface = Interfaces::velocity;
+  Interfaces eff_iface = Interfaces::effort;
+
+  for (const auto & joint : params_.joint_names) {
+
+    jnt_id_.insert(std::make_pair(joint, idx));
+
+    joint_state_interfaces_[idx].reserve(3);
+
+    // Create the state interfaces
+    state_interface_names_[idx].resize(3);
+    state_interface_names_[idx][pos_iface._value] = joint + "/" + pos_iface._to_string();
+    state_interface_names_[idx][vel_iface._value] = joint + "/" + vel_iface._to_string();
+    state_interface_names_[idx][eff_iface._value] = joint + "/" + eff_iface._to_string();
+
+    idx++;
+  }
+  // Position command 
+
+  joint_cmd_sub_ =  get_node()->create_subscription<std_msgs::msg::Float64MultiArray>(
+    "tsid_controllers/joint_position_cmd", 1,
+    std::bind(&JointSpaceTsidController::setPositionCb, this, _1));
+
+  pinocchio::urdf::buildModelFromXML(
+    this->get_robot_description(), pinocchio::JointModelFreeFlyer(), model_);
+
+  RCLCPP_INFO(get_node()->get_logger(), "Model has been built, it has %d joints", model_.njoints);
+
+  std::vector<pinocchio::JointIndex> joints_to_lock;
+  for (auto & name : model_.names) {
+    if (name != "universe" && name != "root_joint" &&
+      std::find(
+        params_.joint_names.begin(), params_.joint_names.end(),
+        name) == params_.joint_names.end())
+    {
+      joints_to_lock.push_back(model_.getJointId(name));
+      RCLCPP_INFO(get_node()->get_logger(), "Lock joint %s: ", name.c_str());
+    }
+  }
+
+  model_ =
+    buildReducedModel(model_, joints_to_lock, pinocchio::neutral(model_));
+
+  for (auto joint : model_.names) {
+    RCLCPP_INFO(get_node()->get_logger(), "Joint name: %s", joint.c_str());
+  }
+  // Getting control period
+  dt_ = rclcpp::Duration(std::chrono::duration<double, std::milli>(1e3 / this->get_update_rate()));
+  RCLCPP_INFO(get_node()->get_logger(), "Control period: %f", dt_.seconds());
+
+  // Initialization of the TSID
+  robot_wrapper_ = new tsid::robots::RobotWrapper(
+    model_,
+    tsid::robots::RobotWrapper::RootJointType::FLOATING_BASE_SYSTEM, true);
+
+  formulation_ = new tsid::InverseDynamicsFormulationAccForce("tsid", *robot_wrapper_, true);
+
+  // Joint Posture Task
+  task_joint_posture_ = new tsid::tasks::TaskJointPosture(
+    "task-joint-posture",
+    *robot_wrapper_);
+  Eigen::VectorXd kp = params_.posture_gain * Eigen::VectorXd::Ones(robot_wrapper_->nv() - 6);
+  Eigen::VectorXd kd = 2.0 * kp.cwiseSqrt();
+  task_joint_posture_->Kp(kp);
+  task_joint_posture_->Kd(kd);
+  int posture_priority = 1;  // 0 constraint, 1 cost function
+  double transition_time = 0.0;
+  double posture_weight = 1e-3;
+  formulation_ ->addMotionTask(*task_joint_posture_, 
+  posture_weight, posture_priority, transition_time);
+
+  Eigen::VectorXd q0 = Eigen::VectorXd::Zero(robot_wrapper_->nv());
+
+  traj_joint_posture_ = new tsid::trajectories::TrajectoryEuclidianConstant(
+    "traj_joint", q0.tail(robot_wrapper_->nv() - 6));
+  task_joint_posture_->setReference(traj_joint_posture_->computeNext());
+
+  // Joint Bounds Task
+  task_joint_bounds_ = new tsid::tasks::TaskJointBounds(
+    "task-joint-bounds",
+    *robot_wrapper_, dt_.seconds());
+  Eigen::VectorXd q_min = model_.lowerPositionLimit.tail(model_.nv - 6);
+  Eigen::VectorXd q_max = model_.upperPositionLimit.tail(model_.nv - 6);
+
+  int bounds_priority = 0;  // 0 constraint, 1 cost function
+  double bounds_weight = 1;
+  // Joint velocity bounds
+  v_scaling_ = params_.velocity_scaling;
+  Eigen::VectorXd v_max = v_scaling_ * model_.velocityLimit.tail(model_.nv - 6);
+  Eigen::VectorXd v_min = -v_max;
+  task_joint_bounds_->setVelocityBounds(v_min, v_max);
+  formulation_->addMotionTask(*task_joint_bounds_, bounds_weight, bounds_priority, transition_time);
+
+  // Initializing solver
+  solver_ = new tsid::solvers::SolverHQuadProgFast("qp solver");
+
+  solver_->resize(formulation_->nVar(), formulation_->nEq(), formulation_->nIn());
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+InterfaceConfiguration JointSpaceTsidController::state_interface_configuration()
+const
+{
+  std::vector<std::string> state_interfaces_config_names;
+
+  for (const auto & joint: params_.joint_names) {
+    for (int i = 0; i < 3; i++) {
+      state_interfaces_config_names.push_back(state_interface_names_[jnt_id_.at(joint)][i]);
+    }
+  }
+  return {
+    controller_interface::interface_configuration_type::INDIVIDUAL, state_interfaces_config_names};
+}
+
+controller_interface::InterfaceConfiguration
+JointSpaceTsidController::command_interface_configuration() const
+{
+
+  std::vector<std::string> command_interfaces_config_names;
+  for (const auto & joint : params_.joint_names) {
+    const auto full_name = joint + "/position";
+    command_interfaces_config_names.push_back(full_name);
+  }
+
+  return {
+    controller_interface::interface_configuration_type::INDIVIDUAL,
+    command_interfaces_config_names};
+
+
+}
+
+
+controller_interface::CallbackReturn JointSpaceTsidController::on_activate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // Check if all the command interfaces are in the actuator interface
+  for (const auto & joint : params_.joint_names) {
+    if (
+      !controller_interface::get_ordered_interfaces(
+        state_interfaces_, state_interface_names_[jnt_id_[joint]],
+        std::string(""),
+        joint_state_interfaces_[jnt_id_[joint]]))
+    {
+      RCLCPP_ERROR(
+        this->get_node()->get_logger(), "Expected %zu state interfaces, got %zu",
+        state_interface_names_[jnt_id_[joint]].size(),
+        joint_state_interfaces_[jnt_id_[joint]].size());
+      return controller_interface::CallbackReturn::ERROR;
+    }
+  }
+
+  // Taking initial state from the joint state interfaces
+  Eigen::VectorXd q0 = Eigen::VectorXd::Zero(robot_wrapper_->nq());
+  Eigen::VectorXd v0 = Eigen::VectorXd::Zero(robot_wrapper_->nv());
+
+  for (const auto & joint : params_.joint_names) {
+    q0.tail(robot_wrapper_->nq() - 7)[model_.getJointId(joint) -
+      2] = joint_state_interfaces_[jnt_id_[joint]][0].get().get_value();
+  }
+
+  formulation_->computeProblemData(0.0, q0, v0);
+
+  // Setting posture task reference as initial position
+  traj_joint_posture_->setReference(q0.tail(robot_wrapper_->nq() - 7));
+  task_joint_posture_->setReference(traj_joint_posture_->computeNext());
+
+
+  RCLCPP_INFO(get_node()->get_logger(), "Initial position: %f", q0[0]);
+
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+controller_interface::CallbackReturn JointSpaceTsidController::on_deactivate(
+  const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  // reset command buffer
+  // TODO: set 0.0 torque to stop it
+  return controller_interface::CallbackReturn::SUCCESS;
+}
+
+
+controller_interface::return_type JointSpaceTsidController::update(
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  updateParams();
+  
+  // Taking current state
+  Eigen::VectorXd q = Eigen::VectorXd::Zero(robot_wrapper_->nq());
+  Eigen::VectorXd v = Eigen::VectorXd::Zero(robot_wrapper_->nv());
+  q[6] = 1.0;
+
+  for (const auto & joint : params_.joint_names) {
+    q.tail(robot_wrapper_->nq() - 7)[model_.getJointId(joint) -
+      2] = joint_state_interfaces_[jnt_id_[joint]][Interfaces::position].get().get_value();
+    v.tail(robot_wrapper_->nv() - 6)[model_.getJointId(joint) -
+      2] = joint_state_interfaces_[jnt_id_[joint]][Interfaces::velocity].get().get_value();
+
+  }
+  
+  // Computing the problem data
+  const tsid::solvers::HQPData solverData = formulation_->computeProblemData(0.0, q, v);
+
+  // Solving the problem
+  const auto sol = solver_->solve(solverData);
+
+  // Integrating acceleration to get velocity
+  Eigen::VectorXd a = formulation_->getAccelerations(sol);
+  Eigen::VectorXd v_cmd = v + a * 0.5 * dt_.seconds();
+
+  // Integrating velocity to get position
+  auto q_int = pinocchio::integrate(model_, q, v_cmd * dt_.seconds());
+
+  auto q_cmd = q_int.tail(model_.nq - 7);
+
+  // Setting the command to the joint command interfaces
+  for (const auto & joint : params_.joint_names) {
+    command_interfaces_[jnt_id_[joint]].set_value(
+      q_cmd[model_.getJointId(joint) - 2]);
+  }
+
+
+  return controller_interface::return_type::OK;
+}
+
+void JointSpaceTsidController::updateParams()
+{
+  if (param_listener_->is_old(params_)) {
+    params_ = param_listener_->get_params();
+    RCLCPP_INFO(get_node()->get_logger(), "Updating parameters");
+
+    task_joint_posture_->Kp(params_.posture_gain * Eigen::VectorXd::Ones(robot_wrapper_->nv() - 6));
+    task_joint_posture_->Kd(2.0 * task_joint_posture_->Kp().cwiseSqrt());
+    v_scaling_ = params_.velocity_scaling;
+    Eigen::VectorXd v_max = v_scaling_ * model_.velocityLimit.tail(model_.nv - 6);
+    Eigen::VectorXd v_min = -v_max;
+    task_joint_bounds_->setVelocityBounds(v_min, v_max);
+  }
+}
+
+void JointSpaceTsidController::setPositionCb(
+  std_msgs::msg::Float64MultiArray::ConstSharedPtr msg)
+{
+      if (msg->data.size() != params_.joint_names.size()) {
+        RCLCPP_ERROR(get_node()->get_logger(), "Received joint position command with incorrect size");
+        return;
+      }
+
+      // Setting the reference
+      Eigen::VectorXd ref(params_.joint_names.size());
+
+      for (size_t i = 0; i < params_.joint_names.size(); i++) {
+        ref[i] = msg->data[i];
+      }
+
+      tsid::trajectories::TrajectorySample sample_posture_joint(ref.size());
+      sample_posture_joint.setValue(ref);
+
+      task_joint_posture_->setReference(sample_posture_joint);
+      
+      auto get_ref = task_joint_posture_->getReference();
+      auto ref_pos = get_ref.getValue();
+      RCLCPP_INFO(
+        get_node()->get_logger(), " Reference position joints : %f %f %f",
+        ref_pos[0], ref_pos[1], ref_pos[2]);
+
+
+}
+
+
+}  // namespace tsid_controllers
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(tsid_controllers::JointSpaceTsidController,
+  controller_interface::ControllerInterface)
